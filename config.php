a92fd670a513f1cc56589771e827db7d7f3b9454
dhianiwidiya <72174625+dhianiwidiya@users.noreply.github.com>
2020-11-26 11:13:17 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/asdosecom/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/asdosecom/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/asdosecom/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/asdosecom/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/asdosecom/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/asdosecom/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/asdosecom/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/asdosecom/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/asdosecom/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/asdosecom/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/asdosecom/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/asdosecom/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/asdosecom/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tokokita');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
